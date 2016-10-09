9c49ac76a0094044631cd0d269b9ddce6a419538
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-09 14:02:47 +0300

    Changes structure

--- chapter_001/src/main/java/com/vgoryashko/Calculate.java
+++ chapter_001/src/main/java/com/vgoryashko/Calculate.java
@@ -1,12 +1,8 @@
 package com.vgoryashko;
 
 public class Calculate{
-	public String echo(String value){
-		return String.format("%s %s %s", value, value, value);
-	}
-	
+
 	public static void main (String[] args){
-		Calculate calc = new Calculate();
-		System.out.print(calc.echo("aah"));
+		System.out.print("Hello world.");
 	}
 }
