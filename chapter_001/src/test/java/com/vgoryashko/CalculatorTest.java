379a84bd0a0d308c762c328fa5629111a39e55d1
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-10 20:29:05 +0300

    Corrected classes Calculator.java and CalculatorTest.java

--- chapter_001/src/test/java/com/vgoryashko/CalculatorTest.java
+++ chapter_001/src/test/java/com/vgoryashko/CalculatorTest.java
@@ -4,14 +4,48 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/** 
+ * Class that performs testing of a class com.vgoryashko.Calculator.
+ * @author vgoryashko
+ * @since 10/10/2016
+ * version 1.0
+ */
+
 public class CalculatorTest{
+	
 	@Test
-	public void whenAddthenSumOfFirstAndSecond(){
+	public void whenAddThenSumOfFirstAndSecond(){
 		//Assign
 		Calculator calc = new Calculator();
-		calc.add(2.0, 2.0);
+		calc.addition(2.0, 2.0);
 		double result = calc.getResult();
 		assertThat(result, is(4.0));
 	}
 	
+	@Test
+	public void whenSubstractionThenSubstractionOfFirstAndSecond(){
+		//Assign
+		Calculator calc = new Calculator();
+		calc.substraction(12.0, 5.0);
+		double result = calc.getResult();
+		assertThat(result, is(7.0));
+	}
+	
+	@Test
+	public void whenMultiplyThenMultiplicationOfFirstAndSecond(){
+		//Assign
+		Calculator calc = new Calculator();
+		calc.multiplication(6.0, 6.0);
+		double result = calc.getResult();
+		assertThat(result, is(36.0));
+	}
+	
+	@Test
+	public void whenDivisionThenDivisionOfFirstAndSecond(){
+		//Assign
+		Calculator calc = new Calculator();
+		calc.division(27.0, 9.0);
+		double result = calc.getResult();
+		assertThat(result, is(3.0));
+	}
 }
