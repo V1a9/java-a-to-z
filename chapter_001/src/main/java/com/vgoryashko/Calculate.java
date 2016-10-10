6a88383940cb651eff1e61743deeecef23b962a0
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-10 11:56:52 +0300

    JavaDoc added to the code

--- chapter_001/src/main/java/com/vgoryashko/Calculate.java
+++ chapter_001/src/main/java/com/vgoryashko/Calculate.java
@@ -1,5 +1,12 @@
 package com.vgoryashko;
 
+/** 
+ * Class for that prints "Hello world."
+ * @author vgoryashko
+ * @since 10/10/2016
+ * version 1.0
+ */
+
 public class Calculate{
 
 	public static void main (String[] args){
