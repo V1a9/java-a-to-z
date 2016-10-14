9f26d1d2522b2a22ea1dfd7f6c99cedd7e3e7732
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-14 04:30:58 +0300

    CalcLongestTriangleSide.java reworked

--- chapter_001/src/main/java/com/vgoryashko/CalcLongestTriangleSide.java
+++ chapter_001/src/main/java/com/vgoryashko/CalcLongestTriangleSide.java
@@ -3,18 +3,17 @@ package com.vgoryashko;
 /** 
  * Class that calculates max length of triangle's side
  * @author vgoryashko
- * @since 13/10/2016
- * version 1.0
+ * @since 14/10/2016
+ * version 1.1
  */
 
 public class CalcLongestTriangleSide{
 	public double calcLongestSide(Point a, Point b, Point c){
-		double maxLength = 0;
-		double[] lengths = new double[]{a.distanceTo(b), b.distanceTo(c), c.distanceTo(a)};
-		for(double length : lengths){
-			if (length >= maxLength)
-				maxLength = length;
-		}
-		return maxLength;
+		double maxLength = a.distanceTo(b);
+		if (b.distanceTo(c) > a.distanceTo(b)){
+			return maxLength = b.distanceTo(c);
+		} else if (c.distanceTo(a) > a.distanceTo(b))
+			return maxLength = c.distanceTo(a);
+			return maxLength;
 	}
 }
