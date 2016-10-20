ea1b33831fc09eb563a04218eaec3d47c6bc0b49
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-20 12:45:07 +0300

    SortArray.java and SortArrayTest.java were added

---
+++ chapter_001/src/main/java/com/vgoryashko/sortarray/SortArray.java
@@ -0,0 +1,26 @@
+package com.vgoryashko;
+
+/* 
+ * Class that sorts a given array of integers
+ * @author vgoryashko
+ * @since 20/10/2016
+ * @version 1.0
+ */
+ 
+ import java.util.*;
+ 
+ public class SortArray{
+	public int[] sort(int[] values){
+		int changeValue;
+		for (int i = 0; i < values.length - 1; i++){
+			for (int j = 0; j < values.length - 1; j++){
+				if (values[j] > values[j + 1]) {
+					changeValue = values[j + 1];
+					values[j + 1] = values[j];
+					values[j] = changeValue;
+				}
+			}
+		}
+		return values;
+	}
+}
