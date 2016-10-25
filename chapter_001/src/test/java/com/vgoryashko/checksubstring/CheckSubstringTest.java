a5eb06b8fcbcfc82f46914d6bdbf833f2d0bd9cc
Vlad Goryashko <vgoryashko@gmail.com>
2016-10-25 19:50:06 +0300

    Updated version of CheckSubstringTest.java with additional tests was added

--- chapter_001/src/test/java/com/vgoryashko/checksubstring/CheckSubstringTest.java
+++ chapter_001/src/test/java/com/vgoryashko/checksubstring/CheckSubstringTest.java
@@ -3,8 +3,8 @@ package com.vgoryashko.checksubstring;
 /* 
  * Class that tests that a given string is a part of an origin string (com.vgoryashko.checksubstring.CheckSubstringTest)
  * @author vgoryashko
- * @since 23/10/2016
- * @version 1.0
+ * @since 25/10/2016
+ * @version 1.1
  */
 
 import org.junit.Test;
@@ -13,8 +13,26 @@ import static org.hamcrest.core.Is.is;
 
 public class CheckSubstringTest{
 	@Test
-	public void testContains(){
+	public void testContainsManyLetters(){
 		CheckSubstring checkSubstring = new CheckSubstring();
 		assertThat(checkSubstring.contains("Substring", "ubstr"), is(true));
+	}
+
+	@Test
+	public void testContainsOneLetter(){
+		CheckSubstring checkSubstring = new CheckSubstring();
+		assertThat(checkSubstring.contains("Substring", "s"), is(true));
+	}
+
+	@Test
+	public void testContainsZeroLetters(){
+		CheckSubstring checkSubstring = new CheckSubstring();
+		assertThat(checkSubstring.contains("Substring", ""), is(false));
+	}
+
+	@Test
+	public void testContainsWrongSymbols(){
+		CheckSubstring checkSubstring = new CheckSubstring();
+		assertThat(checkSubstring.contains("Substring", "uBstr"), is(false));
 	}
 }
