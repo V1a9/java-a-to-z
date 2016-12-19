1ee54055ded9aa95ae26263a25cf0d5f86b4ec9b
Vlad Goryashko <vgoryashko@gmail.com>
2016-12-19 16:53:29 +0200

    ConsoleInputTest was implemented.

--- chapter_002/tracker/src/test/java/com/vgoryashko/tracker/start/ConsoleInputTest.java
+++ chapter_002/tracker/src/test/java/com/vgoryashko/tracker/start/ConsoleInputTest.java
@@ -1,24 +1,29 @@
 package com.vgoryashko.tracker.start;
 
-import org.hamcrest.core.StringContains;
 import org.junit.Test;
-import org.junit.Ignore;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
-import java.io.*;
+import java.io.ByteArrayInputStream;
 import java.lang.String;
 import java.lang.System;
 
 /**
- * Created by Vlad Goryashko on 11/21/2016.
+ * Class class that implements user action of adding a new Bug into the system.
+ * @author Vlad Goryashko
+ * @version 0.2
+ * @since 19/12/2016
  */
 public class ConsoleInputTest {
 
     /**
      * Method for testing of the method ask() in the ConsoleInput class.
      */
-   @Ignore @Test
+   @Test
     public void askTest() {
-
+        String buf = "2,148,289";
+        ByteArrayInputStream in = new ByteArrayInputStream(buf.getBytes());
+        System.setIn(in);
+        Input input = new ConsoleInput();
+        assertThat(input.ask("Please enter number: "), is("2,148,289"));
     }
 }
