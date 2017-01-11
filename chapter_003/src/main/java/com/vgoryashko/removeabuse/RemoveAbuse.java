c08d9096c21d7d8a5abee60963231947e6db8a68
Vlad Goryashko <vgoryashko@gmail.com>
2017-01-11 15:50:53 +0200

    Draft version 0.1 of task 3.1.2 from IO module.

---
+++ chapter_003/src/main/java/com/vgoryashko/removeabuse/RemoveAbuse.java
@@ -0,0 +1,20 @@
+package com.vgoryashko.removeabuse;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+/**
+ * Class that removes abuse words from input stream.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 11.01.2017
+ */
+public class RemoveAbuse {
+
+    public void dropAbuses(InputStream input, OutputStream out, String[] abuse) {
+        Scanner scanner = new Scanner(input);
+
+    }
+}
---
+++ chapter_003/src/main/java/com/vgoryashko/removeabuse/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Domains classes that removes abuse words in input stream.
+ * @author Vlad Goryashko
+ * @since 11.01.2017
+ */
+package com.vgoryashko.removeabuse;
