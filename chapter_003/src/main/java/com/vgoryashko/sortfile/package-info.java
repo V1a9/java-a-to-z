b279a628c4940f314011a6986f2e743cfa7a7fa9
Vlad Goryashko <vgoryashko@gmail.com>
2017-01-16 13:12:04 +0200

    Initial commit of task 3.1.3 from IO module.

---
+++ chapter_003/src/main/java/com/vgoryashko/sortfile/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Domains classes that performs sorting of a file.
+ * @author Vlad Goryashko
+ * @since 16.01.2017
+ */
+package com.vgoryashko.sortfile;
---
+++ chapter_003/src/test/java/com/vgoryashko/sortfile/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Domains classes that perform testing of a class that implements sorting of a file.
+ * @author Vlad Goryashko
+ * @since 16.01.2017
+ */
+package com.vgoryashko.sortfile;
