a159df417c041a2c3ab3a888be811b503ffaf3a5
Vlad Goryashko <vgoryashko@gmail.com>
2017-02-08 18:31:39 +0200

    Final version 0.5 of task 3.1.4 from IO module (check if a word a palindrome).

--- chapter_003/src/main/java/com/vgoryashko/checkpalindrome/CheckPalindrome.java
+++ chapter_003/src/main/java/com/vgoryashko/checkpalindrome/CheckPalindrome.java
@@ -1,32 +1,17 @@
 package com.vgoryashko.checkpalindrome;
 
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Class that performs check if a word is a palindrome.
  *
  * @author Vlad Goryashko
- * @version 0.4
+ * @version 0.5
  * @since 08.02.2017
  */
 public class CheckPalindrome {
-
-    /**
-     * Method that reads words from the console and validate entered words.
-     *
-     * @return String                   a word to be checked
-     * @throws RuntimeException         RuntimeException
-     */
-    public String input() throws RuntimeException {
-        String result = null;
-        try (Scanner scanner = new Scanner(System.in)) {
-            result = scanner.nextLine();
-        } catch (NoSuchElementException nsee) {
-            System.out.println("IOException in input() method.");
-        }
-        return result;
-    }
 
     /**
      * Starting point for the application.
@@ -34,27 +19,33 @@ public class CheckPalindrome {
      * @param args              Standard parameter for the method main()
      */
     public static void main(String[] args) {
-        CheckPalindrome checkPalindrome = new CheckPalindrome();
-        String string = null;
-        String reverseString;
-        StringBuilder stringBuilder;
 
-        do {
-            System.out.println("Please enter a word from 5 letters or 'y' if you want to exit: ");
-            string = checkPalindrome.input().toLowerCase();
-            if (!string.equals("y")) {
-                if (string.length() == 5) {
-                    stringBuilder = new StringBuilder(string);
-                    reverseString = stringBuilder.reverse().toString();
-                    if (string.equals(reverseString)) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            while (true) {
+
+                System.out.print("Enter a word 5 letters length, or 'q' to exit: ");
+                String string = bufferedReader.readLine();
+                String stringToLowerCase = string.toLowerCase();
+
+                    if ("q".equals(string.toLowerCase())) {
+                        System.out.println("Exit!");
+                        System.exit(0);
+                    }
+                    if (string.length() != 5) {
+                        System.out.printf("This word \"%s\" is not 5 letters long, please try again.".concat(System.getProperty("line.separator")), string);
+                    } else if (string.toLowerCase().equals(new StringBuilder(stringToLowerCase).reverse().toString())) {
                         System.out.printf("This word \"%s\" is a palindrome.".concat(System.getProperty("line.separator")), string);
                     } else {
                         System.out.printf("This word \"%s\" is not a palindrome.".concat(System.getProperty("line.separator")), string);
+
                     }
-                } else {
-                    System.out.printf("This word \"%s\" is not 5 letters long, please try again.".concat(System.getProperty("line.separator")), string);
+
+                System.out.println("-----------".concat(System.getProperty("line.separator")));
+
                 }
-            }
-        } while (string != "y");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
