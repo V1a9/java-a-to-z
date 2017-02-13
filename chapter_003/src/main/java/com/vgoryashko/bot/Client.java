f6750526c5072e9cf0901475f4d78bd37cd4ac80
Vlad Goryashko <vgoryashko@gmail.com>
2017-02-13 21:12:38 +0200

    Draft version 0.3 of task 3.2.1 from IO module  (bot).

--- chapter_003/src/main/java/com/vgoryashko/bot/Client.java
+++ chapter_003/src/main/java/com/vgoryashko/bot/Client.java
@@ -1,26 +1,47 @@
 package com.vgoryashko.bot;
 
 import java.io.BufferedReader;
+//import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 /**
  * @author Vlad Goryashko
- * @version 0.2
+ * @version 0.3
  * @since 2/13/2017
  */
 public class Client {
-    Socket socket = new Socket(InetAddress.getByName(ip), port);
-    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    Scanner console = new Scanner(System.in);
-    do {
-        out.println("Hello oracle");
-        String str;
-        while (!(str = in.readLine()).isEmpty()) {
-            System.out.println(str);
+
+    /**
+     * Main method entry point for the application.
+     * @param args                      standard argument for the main method
+     */
+    public static void main(String[] args) {
+
+        String ip = "127.0.0.1";
+        String str = null;
+        int port = 4444;
+
+        try (Socket socket = new Socket(InetAddress.getByName(ip), port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+
+            do {
+                out.println("Hello oracle");
+                str = scanner.nextLine();
+                if (!str.isEmpty()) {
+                    System.out.println(str);
+                    out.write(str);
+                    System.out.println(in.readLine());
+                }
+            } while (true);
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
         }
-    } while ()
+
+    }
 }
--- chapter_003/src/main/java/com/vgoryashko/bot/Server.java
+++ chapter_003/src/main/java/com/vgoryashko/bot/Server.java
@@ -1,27 +1,55 @@
 package com.vgoryashko.bot;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.InputStreamReader;
+//import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * @author Vlad Goryashko
- * @version 0.2
+ * @version 0.3
  * @since 2/13/2017
  */
 public class Server {
 
-
+    /**
+     * Main method entry point for the application.
+     * @param args                      standard argument for the main method
+     */
     public static void main(String[] args) {
-        int port = 5000;
-        try (ServerSocket serverSocket = new ServerSocket(port);
-             PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));) {
 
-        } catch (IOException ioe) {
+        int port = 4444;
+
+        System.out.println("Waiting for a connection.....");
+
+        try (Socket socket = new ServerSocket(port).accept();
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Connection is established successfully!");
+
+            while (true) {
+
+                System.out.println("Waiting for a command ...");
+
+                String ask = in.readLine();
+                System.out.println(ask);
+
+                if ("hello".equals(ask)) {
+                    out.println("Hello, dear friend, I'm an oracle.");
+                    out.println();
+                }
+
+                if ("exit".equals(ask)) {
+                    System.out.println("Server is terminating....");
+                    out.println("Server is terminating....");
+                    break;
+                }
+            }
+
+        } catch (Exception ioe) {
             ioe.printStackTrace();
         }
     }
