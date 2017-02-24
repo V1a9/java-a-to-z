1c2634075c28fc6ffd56dbb8716768b3761db44d
Vlad Goryashko <vgoryashko@gmail.com>
2017-02-24 11:57:45 +0200

    Draft version 0.1 of the task 3.2.2 from IO module (net file manager).

---
+++ chapter_003/src/main/java/com/vgoryashko/netfilemanager/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Domains classes that implement a network file manager.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 2/24/2017
+ */
+package com.vgoryashko.netfilemanager;
---
+++ chapter_003/src/test/java/com/vgoryashko/netfilemanager/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Domains classes that test implementation of a network file manager.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 2/24/2017
+ */
+package com.vgoryashko.netfilemanager;
