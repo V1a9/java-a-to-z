97991821272252c6872e1459b952f69b0b6de8b2
Vlad Goryashko <vgoryashko@gmail.com>
2017-03-22 16:07:02 +0200

    Draft version 0.13 of the task 3.2.2 from IO module (net file manager).

--- chapter_003/src/main/java/com/vgoryashko/netfilemanager/server/Server.java
+++ chapter_003/src/main/java/com/vgoryashko/netfilemanager/server/Server.java
@@ -1,24 +1,31 @@
 package com.vgoryashko.netfilemanager.server;
 
-import java.io.*;
-import java.net.ServerSocket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.ServerSocket;
 
 /**
  * Class that implements main logic of the server application for the network file manager.
  *
  * @author Vlad Goryashko
- * @version 0.11
- * @since 3/17/17
+ * @version 0.12
+ * @since 3/22/17
  */
 public class Server {
 
-    public Socket getSocket() {
-        return this.socket;
-    }
-
+    /**
+     * Variable that referring to a Socket instance.
+     */
     private Socket socket;
 
+    /**
+     * Constructor for the class.
+     * @param aSocket               <code>Socket</code>
+     */
     public Server(Socket aSocket) {
         this.socket = aSocket;
     }
@@ -43,22 +50,20 @@ public class Server {
      */
     private final File tmpFolder = new File(String.format(".%sroot%stmp", FS, FS));
 
+    /**
+     * Variable that used for referring to a file to be downloaded.
+     */
     File dwnFile = null;
+
+    /**
+     * Variable that used for referring to a file to be uploaded.
+     */
     File uplFile = null;
 
     /**
      * Variable that stores a path of a currentFolder folder.
      */
     private File currentFolder = rootFolder;
-
-    /**
-     * Getter for the variable currentFolderFolder.
-     *
-     * @return                              <code>File</code>
-     */
-    public File getCurrentFolder() {
-        return currentFolder;
-    }
 
     /**
      * Method that setups the server environments.
@@ -69,6 +74,10 @@ public class Server {
         tmpFolder.mkdir();
     }
 
+    /**
+     * Method that performs downloading of a file from a client.
+     * @param aIn                       Input stream
+     */
     public void download(DataInputStream aIn) {
 
         try (RandomAccessFile download = new RandomAccessFile(dwnFile, "rw")) {
@@ -85,6 +94,10 @@ public class Server {
         }
     }
 
+    /**
+     * Method that performs uploading of a file to a client.
+     * @param aOut                       Output stream
+     */
     public void upload(DataOutputStream aOut) {
 
         try (RandomAccessFile upload = new RandomAccessFile(uplFile, "rw")) {
@@ -98,6 +111,9 @@ public class Server {
         }
     }
 
+    /**
+     * Method that implements major logic of the Server application.
+     */
     public void start() {
 
         try (DataInputStream in = new DataInputStream(this.socket.getInputStream());
@@ -219,6 +235,11 @@ public class Server {
 
     }
 
+    /**
+     * Main method entry point for the application.
+     *
+     * @param args                      standard argument for the main method
+     */
     public static void main(String[] args) {
 
         ServerSettings serverSettings = new ServerSettings("app.properties");
---
+++ chapter_003/src/test/java/com/vgoryashko/netfilemanager/server/ServerTest.java
@@ -0,0 +1,9 @@
+package com.vgoryashko.netfilemanager.server;
+
+/**
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 3/22/2017
+ */
+public class ServerTest {
+}
