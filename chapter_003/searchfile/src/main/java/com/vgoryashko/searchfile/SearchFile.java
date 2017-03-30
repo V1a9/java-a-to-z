d231932a6fbb8cf95ef12e3c48176b0f0fc66243
Vlad Goryashko <vgoryashko@gmail.com>
2017-03-30 20:20:20 +0300

    Draft version 0.4 of the test task from module 3 IO.

--- chapter_003/searchfile/src/main/java/com/vgoryashko/searchfile/SearchFile.java
+++ chapter_003/searchfile/src/main/java/com/vgoryashko/searchfile/SearchFile.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  *
  * @author Vlad Goryashko
  * @since 30.03.2017
- * @version 0.3
+ * @version 0.4
  */
 public class SearchFile {
 
@@ -23,6 +23,8 @@ public class SearchFile {
     private Path searchDir = null;
     private Path searchFile = null;
     private Path logFile = null;
+    private boolean misprint = false;
+    private boolean writeLog = false;
 
     public void printHelp() {
 
@@ -32,50 +34,63 @@ public class SearchFile {
         System.out.println("Key \"-m\" a file to be searched by a mask.");
         System.out.println("Key \"-f\" a file to be searched by a full name.");
         System.out.println("Key \"-o\" write result to a file followed by a name of a log file.");
-        System.out.println("Example: -d C:\\ -n test.txt -f -o C:\\tmp\\log.txt\n");
         System.out.println("Type \"help\" to see a help menu.");
-        System.out.println("Type \"exit\" to exit from the program.");
+        System.out.println("Type \"exit\" to exit from the program.\n");
+        System.out.println("Example: -d C:\\ -n test.txt -f -o C:\\tmp\\log.txt\n");
         System.out.println();
     }
 
     public String[] splitCommand(String command) {
-        String[] result = null;
-        result = command.split("-");
+        String[] result = command.split("-");
         return result;
     }
 
     public void retrievePaths(String[] commandSplit) {
 
-        if (commandSplit[0].startsWith("d")) {
-            this.searchDir = Paths.get(commandSplit[0].substring(1).trim());
+        if (commandSplit[1].startsWith("d")) {
+            this.searchDir = Paths.get(commandSplit[1].substring(1).trim());
         } else {
             System.out.println("Wrong key entered. Must be \"-d\". Try again.");
+            misprint = true;
         }
 
-        if (commandSplit[1].startsWith("n")) {
-            this.searchFile = Paths.get(commandSplit[1].substring(1).trim());
+        if (commandSplit[2].startsWith("n")) {
+            this.searchFile = Paths.get(commandSplit[2].substring(1).trim());
         } else {
             System.out.println("Wrong key entered. Must be \"-n\". Try again.");
+            misprint = true;
         }
 
-        if (commandSplit[3].startsWith("o")) {
-            this.logFile = Paths.get(commandSplit[3].substring(1).trim());
-        } else {
+        if (commandSplit.length == 5 && commandSplit[4].startsWith("o")) {
+            this.logFile = Paths.get(commandSplit[4].substring(1).trim());
+            writeLog = true;
+        } else if (commandSplit.length == 5 && !commandSplit[4].startsWith("o")) {
             System.out.println("Wrong key entered. Must be \"-o\". Try again.");
+            misprint = true;
         }
     }
 
-    public void start() {
+    public Path search(Path searchDir, Path searchFile) {
+
+        Path result = Paths.get("");
+
+        Path dir = searchDir;
+        Path file = searchFile;
+
+        return result;
+    }
+
+    public void writeLog(Path searchResult) {
 
         Charset charset = Charset.forName("US-ASCII");
 
-//        if (tmp.toFile().exists()) {
-//            File[] files = tmp.toFile().listFiles();
-//            for (File file : files) {
-//                file.delete();
-//            }
-//            tmp.toFile().delete();
-//        }
+        if (tmp.toFile().exists()) {
+            File[] files = tmp.toFile().listFiles();
+            for (File file : files) {
+                file.delete();
+            }
+            tmp.toFile().delete();
+        }
 
         try {
             Files.createDirectory(tmp);
@@ -84,34 +99,60 @@ public class SearchFile {
             System.err.format("Exception: %s", ioe);
         }
 
+        try (BufferedWriter out = Files.newBufferedWriter(logFile, charset)) {
+            out.write(searchResult.toString(), 0, searchResult.toString().length());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
+
+    public void start(Scanner scanner) {
+
         System.out.println("Program that searches a file in a directory.");
         System.out.println("----------------------------------------------");
         printHelp();
 
-        String command = "";
+        String command;
         String[] commandSplit = null;
 
-        try (Scanner scanner = new Scanner(System.in);
-             BufferedWriter out = Files.newBufferedWriter(logFile, charset)) {
 
-            while (!("exit".equals(command))) {
-                System.out.println("Enter a command: ");
-                command = scanner.nextLine();
+        do {
+
+            System.out.println("Enter a command: ");
+
+            command = scanner.nextLine();
+
+            if (!"exit".equals(command)) {
                 if ("help".equals(command)) {
                     printHelp();
                 } else {
                     commandSplit = splitCommand(command);
                 }
+
+                retrievePaths(commandSplit);
+
+                if (misprint) {
+                    continue;
+                } else {
+                    if (writeLog) {
+                        writeLog(search(searchDir, searchFile));
+                    } else {
+                        System.out.println(search(searchDir, searchFile).toString());
+                    }
+                }
             }
-
-            retrievePaths(commandSplit);
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+        } while (!"exit".equals(command));
     }
 
     public static void main(String[] args) {
-        new SearchFile().start();
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            new SearchFile().start(scanner);
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
     }
 }
