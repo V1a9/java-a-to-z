da5a80ace3fe7fa0c72c46efdd2c936e3d3b429d
Vlad Goryashko <vgoryashko@gmail.com>
2017-03-30 16:50:34 +0300

    Draft version 0.3 of the test task from the 3 module (IO).

--- chapter_003/searchfile/src/main/java/com/vgoryashko/searchfile/SearchFile.java
+++ chapter_003/searchfile/src/main/java/com/vgoryashko/searchfile/SearchFile.java
@@ -10,24 +10,72 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
- * Created by vgoryash on 28.03.2017.
+ * Class that performs search of a file in a given directory.
+ *
+ * @author Vlad Goryashko
+ * @since 30.03.2017
+ * @version 0.3
  */
 public class SearchFile {
 
+    private String fs = File.separator;
+    private Path tmp = Paths.get(String.format(".%schapter_003%stmp", fs, fs));
+    private Path searchDir = null;
+    private Path searchFile = null;
+    private Path logFile = null;
+
+    public void printHelp() {
+
+        System.out.println("Format of a command is the following:");
+        System.out.println("Key \"-d\" followed by a name of a dir where a file to be searched.");
+        System.out.println("Key \"-n\" followed by a name of a file to be searched.");
+        System.out.println("Key \"-m\" a file to be searched by a mask.");
+        System.out.println("Key \"-f\" a file to be searched by a full name.");
+        System.out.println("Key \"-o\" write result to a file followed by a name of a log file.");
+        System.out.println("Example: -d C:\\ -n test.txt -f -o C:\\tmp\\log.txt\n");
+        System.out.println("Type \"help\" to see a help menu.");
+        System.out.println("Type \"exit\" to exit from the program.");
+        System.out.println();
+    }
+
+    public String[] splitCommand(String command) {
+        String[] result = null;
+        result = command.split("-");
+        return result;
+    }
+
+    public void retrievePaths(String[] commandSplit) {
+
+        if (commandSplit[0].startsWith("d")) {
+            this.searchDir = Paths.get(commandSplit[0].substring(1).trim());
+        } else {
+            System.out.println("Wrong key entered. Must be \"-d\". Try again.");
+        }
+
+        if (commandSplit[1].startsWith("n")) {
+            this.searchFile = Paths.get(commandSplit[1].substring(1).trim());
+        } else {
+            System.out.println("Wrong key entered. Must be \"-n\". Try again.");
+        }
+
+        if (commandSplit[3].startsWith("o")) {
+            this.logFile = Paths.get(commandSplit[3].substring(1).trim());
+        } else {
+            System.out.println("Wrong key entered. Must be \"-o\". Try again.");
+        }
+    }
+
     public void start() {
 
-        String fs = File.separator;
-        Path tmp = Paths.get(String.format(".%schapter_003%stmp", fs, fs));
-        Path logFile = Paths.get(String.format(".%schapter_003%stmp%slog.txt", fs, fs, fs));
         Charset charset = Charset.forName("US-ASCII");
 
-        if (tmp.toFile().exists()) {
-            File[] files = tmp.toFile().listFiles();
-            for (File file : files) {
-                file.delete();
-            }
-            tmp.toFile().delete();
-        }
+//        if (tmp.toFile().exists()) {
+//            File[] files = tmp.toFile().listFiles();
+//            for (File file : files) {
+//                file.delete();
+//            }
+//            tmp.toFile().delete();
+//        }
 
         try {
             Files.createDirectory(tmp);
@@ -36,18 +84,27 @@ public class SearchFile {
             System.err.format("Exception: %s", ioe);
         }
 
+        System.out.println("Program that searches a file in a directory.");
+        System.out.println("----------------------------------------------");
+        printHelp();
+
         String command = "";
+        String[] commandSplit = null;
+
         try (Scanner scanner = new Scanner(System.in);
              BufferedWriter out = Files.newBufferedWriter(logFile, charset)) {
 
             while (!("exit".equals(command))) {
                 System.out.println("Enter a command: ");
                 command = scanner.nextLine();
-                String[] commandSplit = command.split(" ");
-                Path searchDir = Paths.get(commandSplit[0]);
-                out.write(searchDir.toString());
-                out.newLine();
+                if ("help".equals(command)) {
+                    printHelp();
+                } else {
+                    commandSplit = splitCommand(command);
+                }
             }
+
+            retrievePaths(commandSplit);
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
