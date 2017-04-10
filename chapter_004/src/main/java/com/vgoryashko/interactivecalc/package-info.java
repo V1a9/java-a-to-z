b8a10049f613b2e46de6f37ee152b3ebb281c9d4
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-10 09:53:45 +0300

    Initial commit of the task 4.1 from the module 4 OOD SRP (interactive calculator application).

---
+++ chapter_004/src/main/java/com/vgoryashko/interactivecalc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of the interactive calculator application.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/10/17
+ */
+package com.vgoryashko.interactivecalc;
