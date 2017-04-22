dc8e3568c6070c651922f176f2cfcfe21e8e58b4
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-22 16:21:24 +0300

    Initial commit of the task 5.1.3 from the module Collections Lite (convert arrays).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/convertarrays/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that domains classes that test implementation of the application that converts two-dimensioning arrays to the List<>
+ * and vice verse.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/22/17
+ */
+package com.vgoryashko.collectionslite.convertarrays;
