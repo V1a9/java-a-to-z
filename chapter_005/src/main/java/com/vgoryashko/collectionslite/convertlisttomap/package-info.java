b2092063e2b12ac73a4ce9aa4429ce7f79f7df69
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-23 12:45:17 +0300

    Initial commit v0.1 of the task 5.2.2 from the module Collection Lite (convert List to Map).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/convertlisttomap/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of the class that converts List to Map.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/23/17
+ */
+package com.vgoryashko.collectionslite.convertlisttomap;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/convertlisttomap/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that converts List to Map.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/23/17
+ */
+package com.vgoryashko.collectionslite.convertlisttomap;
