ad043a8b1c28557abdf7e96c3fb8db31e9b82ee0
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-23 11:01:30 +0300

    Initial commit v 0.1 of the task 5.2.1 from the module Collections Lite (convert List of arrays).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/convertarraylist/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement application that converts List of arrays to the List<Integer>.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/23/17
+ */
+package com.vgoryashko.collectionslite.convertarraylist;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/convertarraylist/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that domains classes that test implementation of the application that converts List of arrays
+ * to the List<Integer>.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/23/17
+ */
+package com.vgoryashko.collectionslite.convertarraylist;
