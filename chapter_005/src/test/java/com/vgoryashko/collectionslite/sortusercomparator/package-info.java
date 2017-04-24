59317cf1e11563aec9051205e3bcdb81b66f296f
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-24 15:10:24 +0300

    Remove Initial commit  v0.1 of the task 5.3.2 from the module Collections Lite (Sort User using Comparator).

--- chapter_005/src/test/java/com/vgoryashko/collectionslite/sortusercomparator/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package that domains classes that test classes that sort User using comparator.
- *
- * @author Vlad Goryashko
- * @version 0.1
- * @since 4/24/17
- */
-package com.vgoryashko.collectionslite.sortusercomparator;
