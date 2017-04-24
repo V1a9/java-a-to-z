27fc6a336bd5434bec46642964f4e96429273bb1
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-24 15:07:29 +0300

    Initial commit  v0.1 of the task 5.3.2 from the module Collections Lite (Sort User using Comparator).

---
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/sortusercomparator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test classes that sort User using comparator.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/24/17
+ */
+package com.vgoryashko.collectionslite.sortusercomparator;
