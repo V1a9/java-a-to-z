ac6cb5dde0f832f823884d4fc1e123695192b77d
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-24 18:02:19 +0300

    Initial commit v0.1 of the test task from the module Collections Lite (Implement Map for a Bank).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/mapforbank/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implements Map collection for a bank.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/24/17
+ */
+package com.vgoryashko.collectionslite.mapforbank;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/mapforbank/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of the classes that realize Map collection for a bank.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/24/17
+ */
+package com.vgoryashko.collectionslite.mapforbank;
