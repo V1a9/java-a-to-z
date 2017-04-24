2f7677edb6d301fed608154e658b0a2d69a27a90
Vlad Goryashko <vgoryashko@gmail.com>
2017-04-24 11:47:39 +0300

    Initial commit v0.1 of the task 5.3.1 from the module Collections Lite (Sort User).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/sortuser/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement sort of Users.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/24/17
+ */
+package com.vgoryashko.collectionslite.sortuser;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/sortuser/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of the class that sorts Users.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 4/24/17
+ */
+package com.vgoryashko.collectionslite.sortuser;
