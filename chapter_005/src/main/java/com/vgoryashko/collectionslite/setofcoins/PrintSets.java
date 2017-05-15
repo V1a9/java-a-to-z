d22d2251dd38af583a55fbf84300826a1f753955
Vlad Goryashko <vgoryashko@gmail.com>
2017-05-15 22:33:18 +0300

    Draft version 0.3 of the test task from the module Collections Lite (Print Sets of coins). Partially operating.

--- chapter_005/src/main/java/com/vgoryashko/collectionslite/setofcoins/PrintSets.java
+++ chapter_005/src/main/java/com/vgoryashko/collectionslite/setofcoins/PrintSets.java
@@ -8,8 +8,8 @@ import java.util.List;
  * Class that prints out all possible sets of coins from given array of coins.
  *
  * @author Vlad Goryashko
- * @version 0.1
- * @since 5/4/17
+ * @version 0.3
+ * @since 5/15/17
  */
 public class PrintSets {
 
@@ -27,89 +27,73 @@ public class PrintSets {
 
     private List<Integer> tmp = new ArrayList<>();
 
-    int sum;
-
     /**
      * Method that prints out all possible sets of coins from given array of coins.
      *
      * @param coins initial set of available coins to construct all possible sets.
      * @param amount a sum of coins in a valid set
      */
-    public void printSets(int[] coins, int amount) {
+    public List<List<Integer>> printSets(int[] coins, int amount) {
 
         int sum = amount;
+        int arrayLength = coins.length;
+        tmp.clear();
 
-        if (coins.length == 1) {
+        if (arrayLength == 1) {
+
             while ((sum -= coins[0]) >= 0) {
                 tmp.add(coins[0]);
                 if (sum == 0) {
                     sets.add(new ArrayList<>(tmp));
                 }
             }
-        } else {
+            --arrayLength;
 
+        } else if (arrayLength > 0) {
+
+            tmp.add(coins[coins.length - 1]);
+            sum -= coins[coins.length - 1];
+
+            for (int i = coins.length - 1, j = coins.length - 1; i >= 0 && j >= 0; ) {
+
+                if (i == j) {
+                    tmp.add(coins[i]);
+                    sum -= coins[i];
+                } else {
+                    tmp.add(coins[j]);
+                    sum -= coins[j];
+                }
+
+                if (sum == 0) {
+
+                    sets.add(new ArrayList<>(tmp));
+
+                    if (tmp.get(tmp.size() - 1).equals(tmp.get(tmp.size() - 2)) && j > 0) {
+                        tmp.subList(tmp.size() - 1, tmp.size()).clear();
+                        tmp.add(coins[--j]);
+
+                    } else if (tmp.get(1) != coins[j]) {
+
+                        for (int k = 1; k < tmp.size(); k++) {
+                            if (!tmp.get(k).equals(tmp.get(k + 1)) && tmp.get(k + 1) == coins[j]) {
+                                tmp.remove(k);
+                                tmp.add(k, coins[j]);
+                                break;
+                            }
+                        }
+                    }
+
+                    sum = amount;
+                    for (Integer element : tmp) {
+                        sum -= element;
+                    }
+
+                    if (sum == 0) {
+                       return printSets(Arrays.copyOf(coins, --arrayLength), amount);
+                    }
+                }
+            }
         }
-
-//        if (coins.length > 0) {
-//
-//            int sum = amount;
-//            tmp.clear();
-//            int currentK;
-//
-//            for (int j = coins.length - 1, k = coins.length - 1; j >= 0 && k >= 0;) {
-//
-//                if (j == k) {
-//                    sum -= coins[j];
-//                    tmp.add(coins[j]);
-//                } else if (k < j) {
-//                    sum -= coins[k];
-//                    tmp.add(coins[k]);
-//                }
-//
-//                boolean equal = false;
-//
-//                if (sum == 0) {
-//                    sets.add(new ArrayList<>(tmp));
-//                    if (tmp.size() == 2) {
-//                        if (tmp.get(0) == tmp.get(1)) {
-//                            tmp.subList(tmp.size() - 1, tmp.size()).clear();
-//                        }
-//                    } else {
-//                        for (int l = 1; l < tmp.size() - 1; l++) {
-//                            if (tmp.get(l) != tmp.get(l + 1)) {
-//                                tmp.subList(l, tmp.size()).clear();
-//                                if (k > 0) {
-//                                    tmp.add(coins[--k]);
-//                                    currentK = k;
-//                                } else {
-//                                    tmp.add(coins[0]);
-//                                }
-//                                break;
-//                            } else {
-//                                equal = true;
-//                            }
-//                        }
-//                    }
-//
-//                    if (equal) {
-//                        tmp.subList(tmp.size() - 1, tmp.size()).clear();
-//                        if (k > 0) {
-//                            tmp.add(coins[--k]);
-//                            currentK = k;
-//                        } else {
-//                            tmp.add(coins[0]);
-//                        }
-//                    }
-//
-//                    sum = amount;
-//
-//                    if (k != 0) {
-//                        for (Integer element : tmp) {
-//                            sum -= element;
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        return sets;
     }
 }
--- chapter_005/src/test/java/com/vgoryashko/collectionslite/setofcoins/PrintSetsTest.java
+++ chapter_005/src/test/java/com/vgoryashko/collectionslite/setofcoins/PrintSetsTest.java
@@ -2,11 +2,14 @@ package com.vgoryashko.collectionslite.setofcoins;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that tests implementation of classes that print out all possible sets of coins.
  *
  * @author Vlad Goryashko
- * @version 0.1
+ * @version 0.3
  * @since 5/5/17
  */
 public class PrintSetsTest {
@@ -17,7 +20,12 @@ public class PrintSetsTest {
     @Test
     public void printSetsTest() {
 
-        new PrintSets().printSets(new int[]{1, 5, 10}, 30);
-
+        List<List<Integer>> result = new ArrayList<>(new PrintSets().printSets(new int[]{1, 5, 10}, 40));
+        for (List<Integer> list : result) {
+            for (Integer integer : list) {
+                System.out.print(integer);
+            }
+            System.out.println();
+        }
     }
 }
