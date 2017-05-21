d453032cecbf690d8c9144c5cb5c93fa59d5f8b9
Vlad Goryashko <vgoryashko@gmail.com>
2017-05-21 17:01:38 +0300

    Initial commit version 0.1 of the task 5.1.3 from the Collections Pro module (Iterator for prime numbers);

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/iteratorprimenumbers/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement Iterator that returns prime numbers from a collection.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 5/21/17
+ */
+package com.vgoryashko.collectionspro.iteratorprimenumbers;
