c07d07045469c57cd0464e47fde2cf3396f34a47
Vlad Goryashko <vgoryashko@gmail.com>
2017-05-24 17:20:09 +0300

    Final version 0.2 of the task 5.2.1 from the Collections Pro module for a review (Generics -> Simple Array);

--- chapter_005/src/main/java/com/vgoryashko/collectionspro/simplearray/SimpleArray.java
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/simplearray/SimpleArray.java
@@ -4,10 +4,63 @@ package com.vgoryashko.collectionspro.simplearray;
  * Class that implements simple array (SimpleArray<T>).
  *
  * @author Vlad Goryashko
- * @version 0.1
- * @since 23.05.2017
+ * @version 0.2
+ * @since 24.05.2017
  */
-public class SimpleArray {
+public class SimpleArray<T> {
 
+    /**
+     * Variable that is referring to an array of Objects.
+     */
+    private Object[] simpleArray;
 
+    /**
+     * Variable that is used as an index in the array.
+     */
+    private int index = 0;
+
+    /**
+     * Constructor for the class.
+     *
+     * @param size of an array to be created.
+     */
+    public SimpleArray (int size) {
+        this.simpleArray = new Object[size];
+    }
+
+    /**
+     * Method that adds an object to an array.
+     *
+     * @param object to be added.
+     */
+    public void add(T object) {
+        simpleArray[index++] = object;
+    }
+
+    /**
+     * Method  that gets an object on the requested position.
+     * @param position of the element in the array.
+     * @return {@code T}
+     */
+    public T get(int position) {
+        return (T) simpleArray[position];
+    }
+
+    /**
+     * Method that update an object on the requested position.
+     *
+     * @param position where an object to be updated
+     * @param object to be updated
+     */
+    public void update(int position, T object) {
+        this.simpleArray[position] = object;
+    }
+
+    /**
+     * Method that deletes an object.
+     * @param position of an object to be deleted
+     */
+    public void delete(int position) {
+        simpleArray[position] = null;
+    }
 }
--- chapter_005/src/test/java/com/vgoryashko/collectionspro/simplearray/SimpleArrayTest.java
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/simplearray/SimpleArrayTest.java
@@ -1,14 +1,36 @@
 package com.vgoryashko.collectionspro.simplearray;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 
 /**
- * Class that
+ * Class that tests implementation of the SimpleArray class.
  *
  * @author Vlad Goryashko
- * @version 0.1
- * @since 23.05.2017
+ * @version 0.2
+ * @since 24.05.2017
  */
 public class SimpleArrayTest {
 
+
+    @Test
+    public void whenSimpleArrayOfIntegersCreatedThenOnlyIntegersCanBeUsed() {
+
+        SimpleArray<Integer> simpleArray = new SimpleArray<>(10);
+
+        simpleArray.add(12);
+        assertThat(simpleArray.get(0), is(12));
+
+        simpleArray.add(21);
+        assertThat(simpleArray.get(1), is(21));
+
+        simpleArray.update(1, 33);
+        assertThat(simpleArray.get(1), is(33));
+
+        simpleArray.delete(1);
+        assertNull(simpleArray.get(1));
+    }
 }
