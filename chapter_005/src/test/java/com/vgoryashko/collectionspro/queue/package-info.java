aed83ebb0bedcfd0cee553483293ffde3a2c1b9b
Vlad Goryashko <vgoryashko@gmail.com>
2017-05-31 11:53:56 +0300

    Initial commit version 0.1 of the task 5.3.3 from Collections Pro module (List -> Queue/Dequeue).

---
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/queue/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of queue/dequeue classes.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 5/31/17
+ */
+package com.vgoryashko.collectionspro.queue;
