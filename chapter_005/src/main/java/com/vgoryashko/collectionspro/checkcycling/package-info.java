98f59256203b6a040769447f2f78893052d6cd19
Vlad Goryashko <vgoryashko@gmail.com>
2017-06-01 13:55:42 +0300

    Initial version 0.1 of the task 5.3.4 from Collections Pro module (List -> Check cycling in List).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/checkcycling/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that domains classes that check cycling in Lists.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 01.06.2017
+ */
+
+package com.vgoryashko.collectionspro.checkcycling;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/checkcycling/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that domains classes that test implementation of class that check cycling in Lists.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 01.06.2017
+ */
+
+package com.vgoryashko.collectionspro.checkcycling;
