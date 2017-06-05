56217749d08dea3a59ec1bdfea805c6bcb35987c
Vlad Goryashko <vgoryashko@gmail.com>
2017-06-05 12:56:29 +0300

    Initial commit v 0.1 of the task 5.4.1 from Collections Pro module for a review (Set -> Implement Set).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/set/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement set.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 6/5/17
+ */
+package com.vgoryashko.collectionspro.set;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/set/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of set.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 6/5/17
+ */
+package com.vgoryashko.collectionspro.set;
