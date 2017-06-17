d2eb7445c739063b90425f825b7639526d00e998
Vlad Goryashko <vgoryashko@gmail.com>
2017-06-17 09:23:52 +0300

    Initial commit v0.1 of the task 5.5.8 from Collections Pro module (Map -> reference book).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/referencebook/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implements reference book.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 6/17/17
+ */
+package com.vgoryashko.collectionspro.referencebook;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/referencebook/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of reference book.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 6/17/17
+ */
+package com.vgoryashko.collectionspro.referencebook;
