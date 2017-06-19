ebb71ff2a36c4397d59cc97b05e1c4c099229edb
Vlad Goryashko <vgoryashko@gmail.com>
2017-06-19 16:23:59 +0300

    Draft version 0.1 of the task 5.6.1 from Collections Pro module (Tree -> Simple Tree).

--- chapter_005/src/main/java/com/vgoryashko/collectionspro/tree/Tree.java
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/tree/Tree.java
@@ -1,30 +1,112 @@
 package com.vgoryashko.collectionspro.tree;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * Class that
+ * Class that implements simple tree data structure.
  *
  * @author Vlad Goryashko
  * @version 0.1
  * @since 6/17/17
+ *
+ * @param <E> type of elements to be stored.
  */
 public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
 
-    class Node<E> {
-        List<Node<E>> children = new Tre;
+    /**
+     * Variable that is referring to an instance of Node.
+     */
+    private Node root;
+
+    /**
+     * Class that represents a node of the tree.
+     * @param <E> type of elements to be stored
+     */
+    private class Node<E> {
+        /**
+         * List of child nodes in the node.
+         */
+        List<Node<E>> children = new LinkedList<>();
+
+        /**
+         * Variable that stores a value.
+         */
         E value;
+
+        /**
+         * Constructor for the class.
+         *
+         * @param value to be stored in the node.
+         */
+        Node(E value) {
+            this.value = value;
+        }
     }
 
+    /**
+     * Method that adds an element into the tree.
+     * @param parent parent.
+     * @param child child.
+     * @return {@code boolean}
+     */
     @Override
     public boolean add(E parent, E child) {
-        return false;
+
+        boolean result = false;
+
+        if (root == null) {
+
+            root = new Node(child);
+            result = true;
+
+        } else {
+
+
+
+        }
+
+        return result;
     }
 
+    /**
+     * Method that implements iterator for the tree.
+     *
+     * @return Iterator()
+     */
     @Override
     public Iterator<E> iterator() {
-        return null;
-    }
-}
 
+        return new Iterator<E>() {
+
+            Node current = root;
+            Node branch = root;
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+
+                boolean result = false;
+
+                if (current != null) {
+
+                    result = true;
+
+                } else
+
+                if (!current.children.isEmpty()) {
+
+                    current = current.children.get(index);
+
+                }
+                return result;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
+    }
 }
