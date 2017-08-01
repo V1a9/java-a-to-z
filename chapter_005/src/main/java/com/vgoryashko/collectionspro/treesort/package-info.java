88038180366ce57c8e88cb519052ff0ddd5bb106
Vlad Goryashko <vgoryashko@gmail.com>
2017-08-01 16:56:02 +0300

    Draft version 0.1 of the additional task for Collections Pro module (Tree - implement two swap methods of nodes BFS & DFS).

---
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/treesort/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package that domains class of a node that defines a tree.
+ * This class contains two methods that swap left and right nodes.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 01.08.2017
+ */
+
+package com.vgoryashko.collectionspro.treesort;
---
+++ chapter_005/src/test/java/com/vgoryashko/collectionspro/treesort/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that domains class that tests swap methods in a tree.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 01.08.2017
+ */
+
+package com.vgoryashko.collectionspro.treesort;
