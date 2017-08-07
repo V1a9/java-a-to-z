b057591d110fff0074a8cb39ee27c45ce327f984
Vlad Goryashko <vgoryashko@gmail.com>
2017-08-07 21:07:03 +0300

    Draft version 0.4 of the additional task from Collections Pro module (Test -> Swap nodes). Draft of BFS swap method.

--- chapter_005/src/main/java/com/vgoryashko/collectionspro/treenodesswap/BreadthFirstSearchSwap.java
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/treenodesswap/BreadthFirstSearchSwap.java
@@ -1,12 +1,14 @@
 package com.vgoryashko.collectionspro.treenodesswap;
 
+import java.util.*;
+
 /**
  * Class that implements method that traverses a tree based on breadth-first search algorithm
  * and swaps left and right nodes.
  *
  * @author Vlad Goryashko
- * @version 0.1
- * @since 02.08.2017
+ * @version 0.2
+ * @since 07.08.2017
  *
  * @param <T> to be used within the class.
  */
@@ -14,9 +16,14 @@ package com.vgoryashko.collectionspro.treenodesswap;
 public class BreadthFirstSearchSwap<T> {
 
     /**
-     * Variable that is referring to a current node.
+     * Variable that is referring to a current node (initial value is a root node).
      */
     private Node<T> current;
+
+    /**
+     * Variable that is referring to a stack of Node's pointers.
+     */
+    private Queue<Node<T>> queue = new LinkedList<>();
 
     /**
      * Constructor for the class.
@@ -24,6 +31,7 @@ public class BreadthFirstSearchSwap<T> {
     public BreadthFirstSearchSwap(Node<T> node) {
 
         this.current = node;
+        this.queue.add(node);
 
     }
 
@@ -32,7 +40,21 @@ public class BreadthFirstSearchSwap<T> {
      */
     public void breadthFirstSearchSwap() {
 
+        if (this.queue.size() != 0) {
 
+            this.current = this.queue.remove();
+
+            if (this.current.getLeft() != null) {
+
+                this.queue.add(this.current.getLeft());
+
+            } else if (this.current.getRight() != null) {
+
+                this.queue.add(this.current.getRight());
+
+            }
+
+        }
 
     }
 
