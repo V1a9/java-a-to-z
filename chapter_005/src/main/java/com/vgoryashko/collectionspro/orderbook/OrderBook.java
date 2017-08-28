e863950157234e1a898e1b4d48b9e27731e640ba
Vlad Goryashko <vgoryashko@gmail.com>
2017-08-28 10:17:44 +0300

    Program performs orders aggregation, sorting of depending on SELL/BUY flag and printing results. Matching isn't implemented.

--- chapter_005/src/main/java/com/vgoryashko/collectionspro/orderbook/OrderBook.java
+++ chapter_005/src/main/java/com/vgoryashko/collectionspro/orderbook/OrderBook.java
@@ -1,21 +1,21 @@
 package com.vgoryashko.collectionspro.orderbook;
 
 import java.nio.file.Path;
+import java.util.TreeMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.Map;
 
 /**
  * Class that creates and prints an Order Book.
  *
  * @author Vlad Goryashko
- * @version 0.5
- * @since 8/27/17
+ * @version 0.6
+ * @since 8/28/17
  */
 public class OrderBook {
-
-    /**
-     * Map that stores all orders according to a book parameter.
-     */
-    private Map<String, Map<Integer, Order>> map;
 
     /**
      * Variable that refers to an instance of XmlParser.
@@ -46,6 +46,16 @@ public class OrderBook {
     }
 
     /**
+     * Collection that contains all BUY orders.
+     */
+    private TreeMap<Float, Order> bidBook;
+
+    /**
+     * Collection that contains all SELL orders.
+     */
+    private TreeMap<Float, Order> sellBook;
+
+    /**
      * Method that implements main logic of a program.
      *
      * @throws Exception Exception
@@ -53,9 +63,123 @@ public class OrderBook {
     public void mainLogic() throws Exception {
 
         this.xmlParser.parseFile(this.pathFile, this.handler);
-        this.map = this.handler.getCollection();
 
+        Map<String, HashMap<Integer, Order>> map = this.handler.getCollection();
+
+        Collection<HashMap<Integer, Order>> collection = new LinkedList<>(map.values());
+
+        Collection<Order> orders;
+
+        for (HashMap<Integer, Order> hashMap : collection) {
+
+            orders = new LinkedList<>(hashMap.values());
+
+            for (Order order : orders) {
+
+                if (!order.getOperation().equals("SELL")) {
+
+                    this.placeOrder(order, true);
+
+                } else {
+
+                    this.placeOrder(order, false);
+
+                }
+
+            }
+
+        }
+
+        this.printBook();
+
+    }
+
+    /**
+     * Method that places orders to a final collections based on the BUY/SELL flag.
+     *
+     * @param order order to be added
+     * @param sell flag that defines either a type of order SELL or BUY
+     */
+    public void placeOrder(Order order, boolean sell) {
+
+        final Comparator<Float> ASCEN = new Comparator<Float>() {
+
+            @Override
+            public int compare(Float o1, Float o2) {
+                return (o1 > o2 ? -1 : (o1 < o2 ? 1 : 0));
+            }
+        };
+
+        final Comparator<Float> DESC = new Comparator<Float>() {
+
+            @Override
+            public int compare(Float o1, Float o2) {
+                return (o1 > o2 ? 1 : (o1 < o2 ? -1 : 0));
+            }
+        };
+
+        if (bidBook == null && sellBook == null) {
+
+            bidBook = new TreeMap<>(ASCEN);
+            sellBook = new TreeMap<>(DESC);
+
+        }
+
+        if (!sell) {
+
+            if (!bidBook.containsKey(order.getPrice())) {
+
+                bidBook.put(order.getPrice(), order);
+
+            } else {
+
+                Order tmp = bidBook.get(order.getPrice());
+                bidBook.put(order.getPrice(), new Order(tmp.getBookNumber(), tmp.getOperation(), tmp.getPrice(), tmp.getVolume() + order.getVolume(), tmp.getOrderId()));
+
+            }
+
+        } else {
+
+            if (!sellBook.containsKey(order.getPrice())) {
+
+                sellBook.put(order.getPrice(), order);
+
+            } else {
+
+                Order tmp = sellBook.get(order.getPrice());
+                sellBook.put(order.getPrice(), new Order(tmp.getBookNumber(), tmp.getOperation(), tmp.getPrice(), tmp.getVolume() + order.getVolume(), tmp.getOrderId()));
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Method that prints Order Book into standard output.
+     */
+    public void printBook() {
+
+        Collection<Order> bidCol = bidBook.values();
+        Collection<Order> sellCol = sellBook.values();
+
+        System.out.println("\nBid\nprice\t@\tvolume");
+
+        for (Order order : bidCol) {
+
+            System.out.printf("%.1f%s@%s%d\n", order.getPrice(), "\t", "\t", order.getVolume());
+
+        }
+
+        System.out.println("\nSell\nprice\t@\tvolume");
+
+        for (Order order : sellCol) {
+
+            System.out.printf("%.1f%s@%s%d\n", order.getPrice(), "\t", "\t", order.getVolume());
+
+        }
 
     }
 
 }
+
