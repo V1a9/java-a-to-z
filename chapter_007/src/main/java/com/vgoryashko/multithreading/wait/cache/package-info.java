3c8558bac6c40303e9c7bc0570cb8cac3d036f46
Vlad Goryashko <vgoryashko@gmail.com>
2017-09-27 09:08:47 +0300

    Initial commit.

---
+++ chapter_007/src/main/java/com/vgoryashko/multithreading/wait/cache/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement a cache for storing of models.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 9/27/17
+ */
+package com.vgoryashko.multithreading.wait.cache;
---
+++ chapter_007/src/test/java/com/vgoryashko/multithreading/wait/cache/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of a cache for storing of models.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 9/27/17
+ */
+package com.vgoryashko.multithreading.wait.cache;
