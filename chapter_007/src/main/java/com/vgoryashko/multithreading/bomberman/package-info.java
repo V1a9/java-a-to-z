a564d53a906d266fb63d120a631f975f402b6d92
Vlad Goryashko <vgoryashko@gmail.com>
2017-09-29 11:04:12 +0300

    Initial commit.

---
+++ chapter_007/src/main/java/com/vgoryashko/multithreading/bomberman/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that implement logic of a bomber man game.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 9/29/17
+ */
+package com.vgoryashko.multithreading.bomberman;
---
+++ chapter_007/src/test/java/com/vgoryashko/multithreading/bomberman/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that domains classes that test implementation of a bomber man game logic.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 9/29/17
+ */
+package com.vgoryashko.multithreading.bomberman;
