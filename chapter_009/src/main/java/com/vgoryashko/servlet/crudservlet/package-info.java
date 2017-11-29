07434e6788014b6a2823f5dc67f78ba047f7c730
Vlad Goryashko <vgoryashko@gmail.com>
2017-11-29 12:03:20 +0200

    init commit.

---
+++ chapter_009/src/main/java/com/vgoryashko/servlet/crudservlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that contains classes that implement CRUD servlet.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 11/29/17
+ */
+package com.vgoryashko.servlet.crudservlet;
---
+++ chapter_009/src/test/java/com/vgoryashko/servlet/crudservlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package that contains classes that test implementation of the CRUD servlet.
+ *
+ * @author Vlad Goryashko
+ * @version 0.1
+ * @since 11/29/17
+ */
+package com.vgoryashko.servlet.crudservlet;
